d8036ca59ceba0510e0ccf1f06d7df978328096f
Vladimir Vialov <vyalov.v@gmail.com>
2025-07-20 20:28:59 +0200

    fix keymap

--- src/keymap.rs
+++ src/keymap.rs
@@ -13,8 +13,8 @@ pub const fn get_default_keymap() -> [[[KeyAction; COL]; ROW]; NUM_LAYER] {
             [k!(No), k!(No), k!(No), k!(Space), mo!(1), mo!(1), mo!(2), k!(No), k!(No), k!(No)]
         ]),
         layer!([
-            [wm!(Kp1, ModifierCombination::SHIFT), wm!(Kp2, ModifierCombination::SHIFT), wm!(Kp3, ModifierCombination::SHIFT), wm!(Kp4, ModifierCombination::SHIFT), wm!(Kp5, ModifierCombination::SHIFT), wm!(Kp6, ModifierCombination::SHIFT), wm!(Kp7, ModifierCombination::SHIFT), wm!(Kp8, ModifierCombination::SHIFT), wm!(Kp9, ModifierCombination::SHIFT), wm!(Kp0, ModifierCombination::SHIFT)],
-            [mt!(Kp1, ModifierCombination::CTRL), mt!(Kp2, ModifierCombination::ALT), mt!(Kp3, ModifierCombination::SHIFT), mt!(Kp4, ModifierCombination::GUI), k!(Kp5), k!(Kp6), mt!(Kp7, ModifierCombination::GUI), mt!(Kp8, ModifierCombination::SHIFT), mt!(Kp9, ModifierCombination::ALT), mt!(Kp0, ModifierCombination::CTRL)],
+            [wm!(Kc1, ModifierCombination::SHIFT), wm!(Kc2, ModifierCombination::SHIFT), wm!(Kc3, ModifierCombination::SHIFT), wm!(Kc4, ModifierCombination::SHIFT), wm!(Kc5, ModifierCombination::SHIFT), wm!(Kc6, ModifierCombination::SHIFT), wm!(Kc7, ModifierCombination::SHIFT), wm!(Kc8, ModifierCombination::SHIFT), wm!(Kc9, ModifierCombination::SHIFT), wm!(Kc0, ModifierCombination::SHIFT)],
+            [mt!(Kc1, ModifierCombination::CTRL), mt!(Kc2, ModifierCombination::ALT), mt!(Kc3, ModifierCombination::SHIFT), mt!(Kc4, ModifierCombination::GUI), k!(Kc5), k!(Kc6), mt!(Kc7, ModifierCombination::GUI), mt!(Kc8, ModifierCombination::SHIFT), mt!(Kc9, ModifierCombination::ALT), mt!(Kc0, ModifierCombination::CTRL)],
             [k!(F1), k!(F2), k!(F3), k!(F4), k!(F5), k!(F6), k!(F7), k!(F8), k!(F9), k!(F10)],
             [k!(No), k!(No), k!(No), k!(No), a!(Transparent), k!(F11), k!(F12), k!(No), k!(No), k!(No)]
         ]),
