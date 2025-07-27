2fbeaf406acbcc4f30265eea2e1e63caf5d60568
Vladimir Vialov <vyalov.v@gmail.com>
2025-07-27 20:10:54 +0200

    change layers order

--- src/keymap.rs
+++ src/keymap.rs
@@ -24,7 +24,7 @@ pub const fn get_default_keymap() -> [[[KeyAction; COL]; ROW]; NUM_LAYER] {
             [k!(Q),        k!(W),       k!(E),         k!(R),       k!(T),          k!(Y),  k!(U),       k!(I),         k!(O),       k!(P)],
             [mt!(A, CTRL), mt!(S, ALT), mt!(D, SHIFT), mt!(F, GUI), k!(G),          k!(H),  mt!(J, GUI), mt!(K, SHIFT), mt!(L, ALT), mt!(Semicolon, CTRL)],
             [k!(Z),        k!(X),       k!(C),         k!(V),       k!(B),          k!(N),  k!(M),       k!(Comma),     k!(Dot),     k!(NonusBackslash)],
-            [k!(No),       k!(No),      k!(No),        k!(Space),   mo!(3),         mo!(1), mo!(2),      k!(No),        k!(No),      k!(No)]
+            [k!(No),       k!(No),      k!(No),        k!(Space),   mo!(1),         mo!(2), mo!(3),      k!(No),        k!(No),      k!(No)]
         ]),
         layer!([
             [k!(No),            wm!(Equal, SHIFT), wm!(Kc3, SHIFT), wm!(Kc4, SHIFT),  k!(No),                   k!(Equal),        wm!(Kc0, SHIFT), wm!(Kc9, SHIFT), k!(BrightnessDown), k!(BrightnessUp)],
@@ -33,16 +33,16 @@ pub const fn get_default_keymap() -> [[[KeyAction; COL]; ROW]; NUM_LAYER] {
             [k!(No),            k!(No),            k!(No),          k!(No),           a!(No),                   a!(Transparent),  k!(No),          k!(No),          k!(No),             k!(No)]
         ]),
         layer!([
-            [k!(MediaPrevTrack), k!(MediaPlayPause), k!(MediaNextTrack), k!(AudioVolDown), k!(AudioVolUp),          k!(Escape), k!(CapsLock),    k!(PageUp),   k!(Backspace), k!(Delete)],
-            [k!(F1),             k!(F2),             k!(F3),             k!(F4),           k!(F5),                  k!(Left),   k!(Down),        k!(Up),       k!(Right),     k!(Enter)],
-            [k!(F6),             k!(F7),             k!(F8),             k!(F9),           k!(F10),                 k!(Home),   k!(Tab),         k!(PageDown), k!(End),       k!(No)],
-            [k!(No),             k!(No),             k!(Bootloader),     k!(F11),          k!(F12),                 k!(No),     a!(Transparent), k!(No),       k!(No),        k!(No)]
-        ]),
-        layer!([
             [wm!(Q, ALT),       wm!(Equal, SHIFT), wm!(Kc3, SHIFT),         wm!(Kc4, SHIFT),          wm!(Kc7, SHIFT),          k!(Equal),         wm!(Kc0, SHIFT),  wm!(Kc9, SHIFT), wm!(O, ALT),   wm!(P, ALT)],
             [wm!(A, ALT),       wm!(Slash, SHIFT), wm!(LeftBracket, SHIFT), wm!(RightBracket, SHIFT), wm!(Kc1, SHIFT),          wm!(H, ALT),       k!(RightBracket), k!(LeftBracket), wm!(L, ALT),   wm!(Semicolon, ALT)],
             [wm!(Minus, SHIFT), k!(Minus),         wm!(Comma, SHIFT),       wm!(Dot, SHIFT),          wm!(Kc8, SHIFT),          wm!(Kc5, SHIFT),   wm!(Kc6, SHIFT),  wm!(Comma, ALT), wm!(Dot, ALT), k!(Slash)],
             [k!(No),            k!(No),            k!(No),                  k!(No),                   a!(Transparent),          k!(Backslash),     wm!(Kc2, SHIFT),  k!(No),          k!(No),        k!(No)]
+        ]),
+        layer!([
+            [k!(MediaPrevTrack), k!(MediaPlayPause), k!(MediaNextTrack), k!(AudioVolDown), k!(AudioVolUp),          k!(Escape), k!(CapsLock),    k!(PageUp),   k!(Backspace), k!(Delete)],
+            [k!(F1),             k!(F2),             k!(F3),             k!(F4),           k!(F5),                  k!(Left),   k!(Down),        k!(Up),       k!(Right),     k!(Enter)],
+            [k!(F6),             k!(F7),             k!(F8),             k!(F9),           k!(F10),                 k!(Home),   k!(Tab),         k!(PageDown), k!(End),       k!(No)],
+            [k!(No),             k!(No),             k!(Bootloader),     k!(F11),          k!(F12),                 k!(No),     a!(Transparent), k!(No),       k!(No),        k!(No)]
         ]),
         // layer!([
         //     [k!(No), k!(No), k!(No), k!(No), k!(No), k!(No), k!(No), k!(No), k!(No), k!(No)],
