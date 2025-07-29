5dbc7f22839be5bb884d2ce6b1e44515501be779
Vladimir Vialov <vyalov.v@gmail.com>
2025-07-29 20:59:36 +0200

    change keymap

--- src/keymap.rs
+++ src/keymap.rs
@@ -10,7 +10,7 @@ use rmk::{
 };
 const COL: usize = 10;
 const ROW: usize = 4;
-const NUM_LAYER: usize = 4;
+const NUM_LAYER: usize = 5;
 
 const ALT: ModifierCombination = ModifierCombination::ALT;
 const SHIFT: ModifierCombination = ModifierCombination::SHIFT;
@@ -27,10 +27,10 @@ pub const fn get_default_keymap() -> [[[KeyAction; COL]; ROW]; NUM_LAYER] {
             [k!(No),       k!(No),      k!(No),        k!(Space),   mo!(1),         mo!(2), mo!(3),      k!(No),        k!(No),      k!(No)]
         ]),
         layer!([
-            [k!(No),            wm!(Equal, SHIFT), wm!(Kc3, SHIFT), wm!(Kc4, SHIFT),  k!(No),                   k!(Equal),        wm!(Kc0, SHIFT), wm!(Kc9, SHIFT), k!(No),        k!(No)],
+            [wm!(Q, ALT),       wm!(Equal, SHIFT), wm!(Kc3, SHIFT), wm!(Kc4, SHIFT),  k!(No),                   k!(Equal),        wm!(Kc0, SHIFT), wm!(Kc9, SHIFT), k!(No),        k!(No)],
             [k!(Kc1),           k!(Kc2),           k!(Kc3),         k!(Kc4),          k!(Kc5),                  k!(Kc6),          k!(Kc7),         k!(Kc8),         k!(Kc9),       k!(Kc0)],
             [wm!(Minus, SHIFT), k!(Minus),         k!(No),          k!(No),           wm!(Kc8, SHIFT),          wm!(Kc5, SHIFT),  wm!(Kc6, SHIFT), wm!(Comma, ALT), wm!(Dot, ALT), k!(Slash)],
-            [k!(No),            k!(No),            k!(No),          k!(No),           a!(Transparent),          a!(No),           k!(No),          k!(No),          k!(No),        k!(No)]
+            [k!(No),            k!(No),            k!(No),          mo!(4),           a!(Transparent),          a!(No),           k!(No),          k!(No),          k!(No),        k!(No)]
         ]),
         layer!([
             [wm!(Q, ALT),       wm!(Equal, SHIFT), wm!(Kc3, SHIFT),         wm!(Kc4, SHIFT),          wm!(Kc7, SHIFT),          k!(Equal),         wm!(Kc0, SHIFT),  wm!(Kc9, SHIFT), wm!(O, ALT),   wm!(P, ALT)],
@@ -42,14 +42,14 @@ pub const fn get_default_keymap() -> [[[KeyAction; COL]; ROW]; NUM_LAYER] {
             [k!(MediaPrevTrack), k!(MediaPlayPause), k!(MediaNextTrack), k!(AudioVolDown), k!(AudioVolUp),          k!(Escape), k!(Backspace),   k!(PageUp),   k!(Delete),    k!(BrightnessUp)],
             [k!(F1),             k!(F2),             k!(F3),             k!(F4),           k!(F5),                  k!(Left),   k!(Down),        k!(Up),       k!(Right),     k!(Enter)],
             [k!(F6),             k!(F7),             k!(F8),             k!(F9),           k!(F10),                 k!(Home),   k!(Tab),         k!(PageDown), k!(End),       k!(BrightnessDown)],
-            [k!(No),             k!(No),             k!(Bootloader),     k!(F11),          k!(F12),                 k!(No),     a!(Transparent), k!(No),       k!(No),        k!(No)]
+            [k!(No),             k!(No),             k!(No),             k!(F11),          k!(F12),                 k!(No),     a!(Transparent), k!(No),       k!(No),        k!(No)]
         ]),
-        // layer!([
-        //     [k!(No), k!(No), k!(No), k!(No), k!(No), k!(No), k!(No), k!(No), k!(No), k!(No)],
-        //     [k!(No), k!(No), k!(No), k!(No), k!(No), k!(No), k!(No), k!(No), k!(No), k!(No)],
-        //     [k!(No), k!(No), k!(No), k!(No), k!(No), k!(No), k!(No), k!(No), k!(No), k!(No)],
-        //     [k!(No), k!(No), k!(No), k!(No), k!(No), k!(No), k!(No), k!(No), k!(No), k!(No)]
-        // ]),
+        layer!([
+            [k!(Bootloader), k!(No), k!(No), k!(No), k!(No), k!(No), k!(No), k!(No), k!(No), k!(No)],
+            [k!(No), k!(No), k!(No), k!(No), k!(No), k!(No), k!(No), k!(No), k!(No), k!(No)],
+            [k!(No), k!(No), k!(No), k!(No), k!(No), k!(No), k!(No), k!(No), k!(No), k!(No)],
+            [k!(No), k!(No), k!(No), k!(No), k!(No), k!(No), k!(No), k!(No), k!(No), k!(No)]
+        ]),
     ]
 }
 
